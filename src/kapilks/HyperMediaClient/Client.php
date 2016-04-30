9d9592141c27e8d62c92549f046370d903e0cd09
Kapilks <kapilsingh1@live.com>
2016-05-01 01:56:43 +0530

    Pagination functions added

--- src/kapilks/HyperMediaClient/Client.php
+++ src/kapilks/HyperMediaClient/Client.php
@@ -6,27 +6,32 @@
 	/**
 	* Client
 	*/
-	class Client
+	class Client implements ArrayAccess
 	{
 		private $url_;
 		private $member_;
 		private $attribute_;
 		private $methods_;
 		private $paginated_;
+		private $responseHeaders_;
+		private $paginatedArray_;
 
 
 		function __construct($data)
 		{
+			$this->paginated_ = false;
 			$this->attribute_ = array();
 			$this->methods_ = array();
 
 			if(is_array($data))
 			{
+				// Initialize through data we already have
 				$this->member_ = $data;
 				$this->loadNewState_();
 			}
 			else
 			{
+				// First get data from server then initialize
 				$this->loadNewStateUrl_($data);
 			}
 		}
@@ -40,27 +45,31 @@
 				echo "'$method' action is not defined in current object.";
 		}
 
+
 		private function loadNewStateUrl_($url)
 		{
 			$this->url_ = $url;
-		
+			
+			//echo "$url\n";
+
 			$req = new HttpRequest($this->url_);
 			$req->send();
 			$response = $req->getResponse();
+			$this->responseHeaders_ = $req->getResponseHeaders();
 
-			//print_r($req->getResponseHeaders());
 			// Convert into associated array
 			$this->member_ = json_decode($response, true);
-
 			$this->loadNewState_();
 		}
 		
+
 		private function loadNewState_()
 		{
 			// Set new class attribute and methods specific to this state
 			$allAttribute = array();
 			$allMethod = array();
-			foreach ($this->member_ as $key => $data) 
+
+			foreach($this->member_ as $key => $data) 
 			{
 				if($this->isAttribute_($key))
 				{
@@ -76,6 +85,8 @@
 					$methodName = $this->createMemberName_($key);
 					$methodBody = function() use ($data)
 					{
+						echo "$data\n";
+
 						$args = func_get_args()[0];
 						$data = URITemplate::expand($data, $args);
 
@@ -88,15 +99,80 @@
 
 			$this->attribute_ = $allAttribute;
 			$this->methods_ = $allMethod;
+			
+			$this->paginate_();
 
 			//print_r($this->attribute_);
 			//print_r($this->methods_);
 		}
 
+
+		private function paginate_()
+		{
+			if($this->isPaginationRequire_())
+			{
+				$this->paginated_ = true;
+				$this->paginatedArray_ = array();
+				foreach($this->attribute_ as $value) 
+				{
+					array_push($this->paginatedArray_, new Client($this->member_[$value]));
+				}
+
+				if(isset($this->responseHeaders_['Link']))
+				{
+					$linkHeader = explode(",", $this->responseHeaders_['Link']);
+					foreach($linkHeader as $link) 
+					{
+						// Separate each url and their actions(like 'next', 'prev' etc.) 
+						$match = array();
+						preg_match("#<([^>]+)>; rel=\"([^\"]+)\"#", $link, $match);
+						
+						$target = $match[1];
+						$action = $match[2];
+
+						$methodBody = function() use ($target)
+						{
+							echo "$target\n";
+
+							$args = func_get_args()[0];
+							$data = URITemplate::expand($target, $args);
+
+							return new Client($data);
+						};
+
+						$this->methods_[$action] = \Closure::bind($methodBody, $this, get_class());
+					}
+				}				
+			}
+		}
+
+
+		private function isPaginationRequire_()
+		{
+			// Already paginated and have 'more' result than that can be displayed in 1 request
+			if(isset($this->responseHeaders_['Link']))
+				return true;
+
+			// Not paginated and have results 'less' than that can be displayed in 1 request
+			$limit = count($this->attribute_);
+			
+			// No attribute but may have actions
+			if($limit == 0)
+				return false;
+
+			for($i = 0; $i < $limit; $i++)
+				if($this->attribute_[$i] != $i)
+					return false;
+
+			return true;
+		}
+
+
 		private function isAttribute_($name)
 		{
 			return strpos($name, "_url") === false;
 		}
+
 
 		private function createMemberName_($name)
 		{
@@ -110,10 +186,43 @@
 			return $name;
 		}
 
+
+		///////////////////////////////////////////////////////////////////////////
+		// Public interface
+
+
+		public function offsetSet($offset, $value)
+		{
+			// Setting a index is not allowed
+		}
+
+
+		public function offsetExists($offset)
+		{
+			return $this->paginated_ && isset($this->paginatedArray_[$offset]);
+		}
+
+
+		public function offsetUnset($offset)
+		{
+
+		}
+
+
+		public function offsetGet($offset)
+		{
+			if($this->offsetExists($offset))
+				return $this->paginatedArray_[$offset];
+
+			return null;
+		}
+
+
 		public function getAttributes()
 		{
 			return $this->attribute_;
 		}
+
 
 		public function getActions()
 		{
